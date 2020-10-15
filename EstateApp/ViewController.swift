d146ec65d6e98eca0bceb94ca753092458f2463c
WonderPoPo <chunpo321@gmail.com>
2020-10-15 15:00:31 +0800

    Initial Commit

---
+++ EstateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EstateApp
+//
+//  Created by Chunpo Chan on 2020/10/15.
+//  Copyright © 2020 ChunPo Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
